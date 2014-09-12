7734aa5c1a70eb2d94229ca6c012f935b8efcf51
vasitjuntong <N_vasit@hotmail.com>
2014-09-12 15:53:15 +0700

    v 0.2.2
    -แก้ไข about us

--- app/src/main/java/com/asevensoft/a7002/StartGame.java
+++ app/src/main/java/com/asevensoft/a7002/StartGame.java
@@ -193,9 +193,9 @@ public class StartGame extends Activity{
             getWindow().getDecorView().setSystemUiVisibility(
                 View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                |View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-               |View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
                |View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
                |View.SYSTEM_UI_FLAG_FULLSCREEN
+               |View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
                |View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
             );
         }
